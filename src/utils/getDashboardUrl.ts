7fde18b7b6ff65fa19c94fb53e57f245c61b79a7
David Orozco <orozco.davidj02@gmail.com>
2025-06-26 14:34:17 -0500

    add redirect for sandboxes status

--- src/utils/getDashboardUrl.ts
+++ src/utils/getDashboardUrl.ts
@@ -1,8 +1,11 @@
 const BASE_URL = "https://app.signadot.com";
 
-const getDashboardUrl = (page: string) => {
+const getDashboardUrl = (page: string, query?: string) => {
   const url = new URL(BASE_URL);
   url.pathname = page;
+  if (query) {
+    url.search = query;
+  }
   return url.toString();
 };
 
