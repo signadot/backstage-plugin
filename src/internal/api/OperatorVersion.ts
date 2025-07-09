3dbc18d86a5297eecc983677d70114366c3794b1
David Orozco <orozco.davidj02@gmail.com>
2025-07-09 12:24:02 -0500

    change last version logic calc

--- src/internal/api/OperatorVersion.ts
+++ src/internal/api/OperatorVersion.ts
@@ -1,34 +1,19 @@
-export type OperatorVersion = {
-  major: number;
-  minor: number;
-  bugfix: number;
-};
-
-export const parseOperatorVersion = (operatorVersion: string): OperatorVersion => {
-  // eslint-disable-next-line prefer-const
-  let [major, minor, bugfix] = operatorVersion.split(".", 3);
-  const hashStart = bugfix.indexOf("-");
-  // We have no way to determine if any given hash is for an earlier or later operator.
-  // So we ignore it and only consider the major, minor, and bugfix numbers.
-  if (hashStart > -1) {
-    bugfix = bugfix.substring(0, hashStart);
-  }
-  return {
-    major: parseInt(major, 10),
-    minor: parseInt(minor, 10),
-    bugfix: parseInt(bugfix, 10),
-  };
-};
+import semver from "semver";
 
 export const isObservedVersionLatest = (
-  observed: OperatorVersion,
-  latest: OperatorVersion
+  observed: string | null,
+  latest: string
 ): boolean => {
-  if (observed.major < latest.major) {
+  if (!observed) {
     return false;
   }
-  if (observed.minor < latest.minor) {
+
+  const coarsedObserved = semver.clean(observed);
+  const coarsedLatest = semver.clean(latest);
+
+  if (!coarsedObserved || !coarsedLatest) {
     return false;
   }
-  return observed.bugfix >= latest.bugfix;
-}; 
+
+  return semver.gte(coarsedObserved.toString(), coarsedLatest.toString());
+};
